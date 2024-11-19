8b8d9ab5c1d20543bf6c297f0aec3f7cc572a8cc
Ibraim <ibraim99.5@gmail.com>
2024-11-19 17:20:14 -0300

    Corrigido caminho para imagens

--- dist/portfolio/server/main.server.mjs
+++
@@ -1,2 +0,0 @@
-import './polyfills.server.mjs';
-import{a}from"./chunk-ZI7U6AVU.mjs";import"./chunk-F7EYYUOT.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
--- dist/server/main.server.mjs
+++ dist/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-ZI7U6AVU.mjs";import"./chunk-F7EYYUOT.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
+import{a}from"./chunk-DC65XTXK.mjs";import"./chunk-F7EYYUOT.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
