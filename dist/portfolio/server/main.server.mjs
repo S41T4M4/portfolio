6dc036c46b66f303a34b44389ffa4d55dcdbfa13
Ibraim <ibraim99.5@gmail.com>
2024-11-21 16:06:34 -0300

    Atualizando build para GitHub Pages

--- dist/portfolio/server/main.server.mjs
+++ dist/portfolio/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-RY4KOOSB.mjs";import"./chunk-F7EYYUOT.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
+import{a}from"./chunk-2LINN3MQ.mjs";import"./chunk-F7EYYUOT.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
